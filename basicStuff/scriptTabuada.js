3ac019d92325a3b222d1f2eed19812ef0a9bb375
Bruno Guerra <gguerrabruno@outlook.com>
2018-08-15 16:28:37 -0300

    Initial commit, adding files

---
+++ basicStuff/scriptTabuada.js
@@ -0,0 +1,9 @@
+for(var i = 1; i<=9; i++){ //Define de qual número será feita a tabuada
+
+    for(var j=1; j<=10; j++){ // Define o número que será multiplicado pelo valor de i
+
+        console.log(i + ' x ' + j + ' = ' + (i*j));
+
+    }
+
+}
